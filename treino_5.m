48264d2e1f2b785b008c91a2c4cc11b8684a2150
jorgermduarte <joka867@hotmail.com>
2023-05-01 23:36:47 +0100

    feat: added treino_5 model

---
+++ treino_5.m
@@ -0,0 +1,66 @@
+% Carregar imagens do diretório e preparar os dados de treinamento
+dataFolder = 'data/datasets';
+startFolder = fullfile(dataFolder, 'start');
+trainFolder = fullfile(dataFolder, 'train1');
+classFolders = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'add', 'div', 'mul', 'sub'};
+
+% Carregar imagens da pasta "start" e pré-processar
+[startImages, startLabels] = loadAndPreprocessImages(startFolder, classFolders);
+
+% Carregar imagens da pasta "train1" e pré-processar
+[trainImages, trainLabels] = loadAndPreprocessImages(trainFolder, classFolders);
+
+% Converter rótulos para dados categóricos
+startLabels = categorical(startLabels, 1:numel(classFolders), classFolders);
+trainLabels = categorical(trainLabels, 1:numel(classFolders), classFolders);
+
+% Criar e treinar a rede neural
+inputSize = [150, 150];
+numClasses = numel(classFolders);
+
+layers = [
+    imageInputLayer(inputSize, 'Normalization', 'none')
+    convolution2dLayer(5, 16, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    convolution2dLayer(5, 32, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    convolution2dLayer(5, 64, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    fullyConnectedLayer(512)
+    dropoutLayer(0.5)
+    fullyConnectedLayer(numClasses)
+    softmaxLayer
+    classificationLayer];
+
+options = trainingOptions('sgdm', ...
+    'InitialLearnRate', 0.01, ...
+    'LearnRateSchedule', 'piecewise', ...
+    'LearnRateDropFactor', 0.2, ...
+    'LearnRateDropPeriod', 5, ...
+    'MaxEpochs', 20, ...
+    'MiniBatchSize', 64, ...
+    'Verbose', false, ...
+    'Plots', 'training-progress', ...
+    'Shuffle', 'every-epoch', ...
+    'ExecutionEnvironment', 'auto', ...
+    'ValidationData', {startImages, startLabels}, ...
+    'ValidationFrequency', 30);
+
+net = trainNetwork(trainImages, trainLabels, layers, options);
+
+% Testar a rede com imagens da pasta "start"
+YPred = classify(net, startImages);
+accuracy = sum(YPred == startLabels) / numel(startLabels);
+fprintf('Accuracy: %.2f\n', accuracy);
+
+% Salvar a rede treinada
+save('models/trained-model-5.mat', 'net');
